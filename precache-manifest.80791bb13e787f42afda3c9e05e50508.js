d1c0f200435a33ff1a23a6c884f1459cc7242570
Boralu <93267@fhss.sjp.ac.lk>
2021-06-20 23:48:00 +0530

    Updates

---
+++ precache-manifest.80791bb13e787f42afda3c9e05e50508.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6de8ea5ae9b8f70070800d5929b85c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c27d283447fb262dc44e",
+    "url": "/static/css/main.a90790e1.chunk.css"
+  },
+  {
+    "revision": "d74018fed2872d5117f4",
+    "url": "/static/js/2.11e38c3f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.11e38c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27d283447fb262dc44e",
+    "url": "/static/js/main.613491b0.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "eb36d53f68ef5cbd72634a53d430b95a",
+    "url": "/static/media/profile.eb36d53f.jpg"
+  },
+  {
+    "revision": "a1e7274ce8156eec7f7b377bb2b749aa",
+    "url": "/static/media/resume.a1e7274c.pdf"
+  }
+]);
